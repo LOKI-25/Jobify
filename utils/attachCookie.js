4f4102553a298957315bb4f6e3f80f7058ee9119
Lokesh Narayana <plokeshn25@gmail.com>
2023-06-23 01:58:44 +0530

    Update attachCookie.js

--- utils/attachCookie.js
+++ utils/attachCookie.js
@@ -1,24 +1,13 @@
 const attachCookie = ({ res, token }) => {
-  // const oneDay = 1000 * 60 * 60 * 24;
+  const oneDay = 1000 * 60 * 60 * 24;
 
-  // res.cookie('token', token, {
-  //   httpOnly: false,
-  //   expires: new Date(Date.now() + oneDay),
-  //   // secure: process.env.NODE_ENV === 'production',
-  //   secure: false,
-  // });
-
-    const cookieOptions = {
+  res.cookie('token', token, {
     domain: 'jobify-ujf0.onrender.com',
-    httpOnly: flase,
-    expires: new Date(Date.now() + 3600000), // Expiration time
-    // Other options...
-  };
-  console.log("Generated Cookie in attachcookie.js");
-  console.log(res);
-  
-  
-  res.setHeader('Set-Cookie', cookie.serialize('token', token, cookieOptions));
+    httpOnly: false,
+    expires: new Date(Date.now() + oneDay),
+    // secure: process.env.NODE_ENV === 'production',
+    secure: false,
+  });
   
 };
 

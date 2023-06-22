d3f5c43e6ee1d426b7b5c8feaa75f37ad4184eb1
Lokesh Narayana <plokeshn25@gmail.com>
2023-06-23 01:52:44 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,8 +46,6 @@ const login = async (req, res) => {
   attachCookie({ res, token });
   console.log("Cookie is attached");
   console.log("RESPONSE FROM LOGIN CONTROLLER line48 in authcontroller.js");
-  req.headers.authorization = token;
-  console.log(`after adding to header `);
   console.log(req);
 
   user.password = undefined;

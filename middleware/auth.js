be0c3ea76a07df92fab4b532d6fc9d336f758cf3
Lokesh Narayana <plokeshn25@gmail.com>
2023-06-23 00:51:22 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ import { UnAuthenticatedError } from '../errors/index.js';
 const auth = async (req, res, next) => {
   const token = req.cookies.token;
   console.log("in Auth.js line 5");
-  console.log(req);
+  console.log(req.cookies);
   console.log(" in auth.js line 6 ???????????"+token);
   if (!token) {
     throw new UnAuthenticatedError('Authentication Invalid??????????????(no token)');
